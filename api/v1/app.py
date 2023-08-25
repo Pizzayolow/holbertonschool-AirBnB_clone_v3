5b186db546c61ef02f70db769a7ab8a1bd31d79c
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-25 11:20:40 +0200

    places reviews and CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,13 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask import Blueprint
 import os
 from flask import jsonify
+from flask_cors import CORS
 
 """create an instance of Flask"""
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 """register a blueprint"""
 app.register_blueprint(app_views)
@@ -21,13 +22,13 @@ def teardown(err):
 
 
 @app.errorhandler(404)
-def not_found(e):
-    """message for 404"""
+def page_not_found(e):
+    """method to return a 404 error"""
     return jsonify(error="Not found"), 404
 
 
 """define with the ENV the port and host"""
 if __name__ == "__main__":
-    host_hbnb = os.environ.get('HBNB_API_HOST', "0.0.0.0")
-    port_hbnb = int(os.environ.get('HBNB_API_PORT', 5000))
+    host_hbnb = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port_hbnb = int(os.environ.get("HBNB_API_PORT", 5000))
     app.run(host=host_hbnb, port=port_hbnb, threaded=True)
